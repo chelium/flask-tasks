65d4c2f2b2a022b2e627766972d2894d6b74b1a3
heliumtank <czhe2@illinois.edu>
2017-07-30 00:14:38 -0500

    Add flask_sqlalchemy and config file

--- goalie/app.py
+++ goalie/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+from goalie.extensions import db
+
 from goalie.main import main_pages
 from goalie.task import task_pages
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('goalie.settings')
     register_blueprints(app)
     register_extensions(app)
     return app
@@ -14,4 +17,4 @@ def register_blueprints(app):
     app.register_blueprint(task_pages)
 
 def register_extensions(app):
-    pass
+    db.init_app(app)
