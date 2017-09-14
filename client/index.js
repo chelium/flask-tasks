b8c6c9d7d0b686a17a1b88d0b4266b244d25afbc
Chris He <chelix@client44-98.wifi.siue.edu>
2017-09-14 11:12:30 -0500

    Switching from React to VueJS

--- client/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import App from './App.js';
-
-injectTapEventPlugin();
-
-ReactDOM.render(<App/>, document.getElementById('root'));
